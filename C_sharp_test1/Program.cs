0b92b4f22107950744fa447c44ebf108e4f1527d
gh26300 <gh26300@gmail.com>
2017-03-17 10:03:20 +0800

    homework2017-03-17

---
+++ C_sharp_test1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace C_sharp_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String URLString = " http://ibus.tbkc.gov.tw/xmlbus/StaticData/GetRoute.xml";
+            XmlTextReader reader = new XmlTextReader(URLString);
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    
+                    case XmlNodeType.Element: // The node is an element.
+                        Console.Write("<" + reader.Name);
+
+                        while (reader.MoveToNextAttribute()) // Read the attributes.
+                        Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+                        Console.Write(">");
+                        Console.WriteLine(">");
+                        break;
+                 
+                   /*
+                    case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine(reader.Value);
+                        break;
+                   */
+                        /*
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                    */
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
