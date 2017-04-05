e9d470f48b2a5e2a4db8003e4685191c8c4eb947
gh26300 <gh26300@gmail.com>
2017-04-05 14:06:36 +0800

    upload &test

---
+++ homework1/README.md
@@ -0,0 +1,3 @@
+# KUAS_multimedia
+KUAS_多媒體系統
+# 課程日期 2017/03/10
---
+++ homework1/hw/C_sharp_test1/State_class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_test1
+{
+    public  class State
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ddesc { get; set; }
+        public string nameZh { get; set; }
+        public string departureZh { get; set; }
+        public string destinationZh { get; set; }
+        public string Id { get; set; }
+        public DateTime CreateTime { get; set; }
+
+    }
+}
