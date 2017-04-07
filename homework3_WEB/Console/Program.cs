fb293b4b9b0a7df0290b2d65e304b98030911f74
gh26300 <gh26300@gmail.com>
2017-04-07 10:23:54 +0800

    work3 program create

---
+++ homework3_WEB/Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using PHC.Modles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHC.ConsoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var import = new PHC.Services.ImportService();
+            var db = new PHC.Repository.stateRepo();
+            var states = import.findState(@"http://ibus.tbkc.gov.tw/xmlbus/StaticData/GetRoute.xml");
+            showState(states);
+            //                .Where(x=>x.CreateTime>DateTime.Now.AddDays(-1))
+//                .Where(x=>x.Id.Length==10)
+            db.CreateDatabase(states);
+            /*states.ToList().ForEach(state => {
+                db.CreateDatabase(states);
+            });*/
+            Console.ReadKey();
+            
+        }
+        public static void showState(List<State> states)
+        {
+            Console.WriteLine(string.Format("共收到{0}筆監測站的資料", states.Count));
+            states.ForEach(x =>
+            {
+                Console.WriteLine(string.Format("編號 {0} , 路徑 {1}", x.nameZh, x.ddesc));
+            });
+        }
+    }
+}
---
+++ homework3_WEB/models/Services/ImportService.cs
@@ -0,0 +1,47 @@
+﻿using PHC.Modles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PHC.Services
+{
+   public class ImportService
+    {
+
+     
+       public List<State> findState(string URLString)
+       {
+           //String URLString = " http://ibus.tbkc.gov.tw/xmlbus/StaticData/GetRoute.xml";
+           List<State> States = new List<State>();
+           var xml = XElement.Load(URLString);
+           var stateNode = xml.Descendants("Route").ToList();
+           for (var i = 0; i < stateNode.Count(); i++)
+           {
+               var statesNode = stateNode[i];
+           }
+           stateNode.Where(x => x.IsEmpty).ToList().ForEach(statesNode =>
+           {
+               var Id = statesNode.Attribute("ID").Value.Trim();
+               var destinationZh = statesNode.Attribute("destinationZh").Value.Trim();
+               var departureZh = statesNode.Attribute("departureZh").Value.Trim();
+               var nameZh = statesNode.Attribute("nameZh").Value.Trim();
+               var ddesc = statesNode.Attribute("ddesc").Value.Trim();
+
+               State stateData = new State();
+               stateData.Id = Id;
+               stateData.ddesc = ddesc;
+               stateData.departureZh = departureZh;
+               stateData.destinationZh = destinationZh;
+               stateData.nameZh = nameZh;
+               stateData.CreateTime = DateTime.Now;
+               States.Add(stateData);
+           });
+
+           return States;
+       }
+
+    }
+}
